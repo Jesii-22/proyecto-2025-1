7c10adaad97f5e631ee64159faa174068db13d6e
Jesii-22 <jesimarzeniuk@gmail.com>
2025-06-05 19:14:48 -0300

    cambios2

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Reemplaza esto con tu API key de Google Gemini
+// API key de Google Gemini
 const GEMINI_API_KEY = 'AIzaSyCwFKPO-Zec8uIG2Sl5zAAr9SMPoOdtyMc';
 const GEMINI_API_URL = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent';
 
